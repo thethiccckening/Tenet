5e9dbdf464ccebd5602cab4206072922cc3037ae
Sekou.Diaby <diabilasekou@gmail.com>
2022-11-10 00:33:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/testapp/Signup.java
@@ -0,0 +1,25 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Signup extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+
+
+    }
+}
