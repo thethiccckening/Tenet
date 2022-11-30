c7489e24345e9c91c9e22737665de3a6fe33a40c
YSaintL00 <dfaseyi.web@gmail.com>
2022-11-29 20:23:03 -0500

    worked on SignUp and SignIn and changed the launcher act to signUp

--- app/src/main/java/com/example/testapp/Settings.java
+++ app/src/main/java/com/example/testapp/Settings.java
@@ -3,12 +3,47 @@ package com.example.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class Settings extends AppCompatActivity {
+
+    //debugging
+    protected static final String ACTIVITY_NAME = "LoginActivity"; //debugging message
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
 }
+
