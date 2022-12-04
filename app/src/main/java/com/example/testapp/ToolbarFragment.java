178d45ae7d763d0735752c75448e45185b585c0a
Patrick Maldonado <mald7690@mylaurier.ca>
2022-12-04 18:44:17 -0500

    Finished ToolbarFragment
    Implemented onClicks as well as fragment ui to be used as toolbar in all activities

--- app/src/main/java/com/example/testapp/ToolbarFragment.java
+++ app/src/main/java/com/example/testapp/ToolbarFragment.java
@@ -1,5 +1,6 @@
 package com.example.testapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,59 +8,52 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ToolbarFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ToolbarFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ToolbarFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ToolbarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ToolbarFragment newInstance(String param1, String param2) {
-        ToolbarFragment fragment = new ToolbarFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.toolbar_fragment, container, false);
+        View view = inflater.inflate(R.layout.toolbar_fragment, container, false);
+        ImageView map = view.findViewById(R.id.map_button);
+        ImageView search = view.findViewById(R.id.search_button);
+        ImageView chat = view.findViewById(R.id.chat_button);
+        ImageView settings = view.findViewById(R.id.settings_button);
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent map1 = new Intent(getActivity(),Map.class);
+                startActivity(map1);
+            }
+        });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent search1 = new Intent(getActivity(),Search.class);
+                startActivity(search1);
+            }
+        });
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chat1 = new Intent(getActivity(),Chat.class);
+                startActivity(chat1);
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent settings1 = new Intent(getActivity(),Settings.class);
+                startActivity(settings1);
+            }
+        });
+
+        return view;
+
+
+
     }
 }
